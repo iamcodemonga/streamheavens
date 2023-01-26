d7b834215f7f7faad71e690f0ea42ac527f2c866
iamcodemonga <codemonga@gmail.com>
2023-01-26 15:06:01 +0100

    initialized state management using redux toolkit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import ScrollToTop from './components/ScrollToTop';
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
 import '/node_modules/bootstrap/dist/js/bootstrap';
 import './index.css';
+
+const store = configureStore({
+  reducer: {}
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
         <ScrollToTop />
-        <App />
+        <Provider store={store}>
+          <App />
+        </Provider>
     </BrowserRouter>
   </React.StrictMode>
 );
