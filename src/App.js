783b97bb68d28b5504d81d9def5dfb10893ea083
unknown <codemonga@gmail.com>
2023-08-31 15:21:29 +0100

    modified the tv shows page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 // main pages routes
 import Home from "./pages/main/Home";
 import Movies from "./pages/main/Movies";
-// import TvShows from "./pages/main/TvShows";
+import TvShows from "./pages/main/TvShows";
 // import Pricing from "./pages/main/Pricing";
 // import About from "./pages/main/About";
 // authentification pages routes
@@ -39,8 +39,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="movies" element={<Movies/>} />
-          {/* <Route path="tvshows" element={<TvShows/>} />
-          <Route path="pricing" element={<Pricing/>} />
+          <Route path="tvshows" element={<TvShows/>} />
+          {/* <Route path="pricing" element={<Pricing/>} />
           <Route path="about" element={<About/>} />
           <Route path="screen" element={<Screen/>} />
           <Route path="register" element={ <Register/> } />
