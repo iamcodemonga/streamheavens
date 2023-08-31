aff58ac37877b6f8194bf278d85b59b89e6cfff7
unknown <codemonga@gmail.com>
2023-08-31 15:43:08 +0100

    modified the about page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./pages/main/Home";
 import Movies from "./pages/main/Movies";
 import TvShows from "./pages/main/TvShows";
 import Pricing from "./pages/main/Pricing";
-// import About from "./pages/main/About";
+import About from "./pages/main/About";
 // authentification pages routes
 // import Register from "./pages/auth/Register";
 // import Login from "./pages/auth/Login";
@@ -41,8 +41,8 @@ function App() {
           <Route path="movies" element={<Movies/>} />
           <Route path="tvshows" element={<TvShows/>} />
           <Route path="pricing" element={<Pricing/>} />
-          {/* <Route path="about" element={<About/>} />
-          <Route path="screen" element={<Screen/>} />
+          <Route path="about" element={<About/>} />
+          {/* <Route path="screen" element={<Screen/>} />
           <Route path="register" element={ <Register/> } />
           <Route path="login" element={ <Login/> } />
           <Route path="forgotpassword" element={ <ForgotPassword/> } />
