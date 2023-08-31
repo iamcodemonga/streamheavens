96b5d4de24710824aa8d28f4ee76ab5dd5205888
unknown <codemonga@gmail.com>
2023-08-31 18:01:53 +0100

    modified all auth pages

--- src/App.js
+++ src/App.js
@@ -7,33 +7,13 @@ import TvShows from "./pages/main/TvShows";
 import Pricing from "./pages/main/Pricing";
 import About from "./pages/main/About";
 // authentification pages routes
-// import Register from "./pages/auth/Register";
-// import Login from "./pages/auth/Login";
-// import ForgotPassword from "./pages/auth/ForgotPassword";
-// import Reset from "./pages/auth/ResetPassword";
-//  user profile routes
-// import Profile from "./pages/user/Profile";
-// import Details from "./components/UserDetails";
-// import EditProfile from "./components/UserEdit";
-// import Favourites from "./components/UserFavourites";
-// import ChangePassword from "./components/UserPassword";
-//others pages setup
-// import Screen from "./pages/other/Screen";
-//required packages and components
+import Register from "./pages/auth/Register";
+import Login from "./pages/auth/Login";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import Reset from "./pages/auth/ResetPassword";
 // import Loader from './components/Loaders/PageLoader'
-// import { useSelector, useDispatch } from "react-redux";
-// import { Navigate } from 'react-router-dom';
-// import { fetchUser } from './features/authSlice';
 
 function App() {
-
-  // const dispatch = useDispatch();
-  // const auth = useSelector((state) => state.auth);
-
-  // useEffect(() => {
-  //   dispatch(fetchUser())
-  // }, [dispatch])
-  
   return (
     <div className="App">
         <Routes>
@@ -42,24 +22,11 @@ function App() {
           <Route path="tvshows" element={<TvShows/>} />
           <Route path="pricing" element={<Pricing/>} />
           <Route path="about" element={<About/>} />
-          {/* <Route path="screen" element={<Screen/>} />
           <Route path="register" element={ <Register/> } />
           <Route path="login" element={ <Login/> } />
           <Route path="forgotpassword" element={ <ForgotPassword/> } />
-          <Route path="reset" element={ <Reset/> } /> */}
-          {/* {auth.user ? <Route path=":id" element={ <Profile/> } >
-              <Route index element={ <Details/> } />
-              <Route path="favourites" element={ <Favourites/> } />
-              <Route path="edit" element={ <EditProfile/> } />
-              <Route path="password" element={ <ChangePassword/> } />
-          </Route> : <Route path=":id" element={ <Navigate to='/' /> } >
-              <Route index element={ <Navigate to='/' /> } />
-              <Route path="favourites" element={ <Navigate to='/' /> } />
-              <Route path="edit" element={ <Navigate to='/' /> } />
-              <Route path="password" element={ <Navigate to='/' /> } />
-          </Route>} */}
+          <Route path="reset" element={ <Reset/> } />
         </Routes>
-      
     </div>
   );
 }
--- src/components/forms/RegisterForm.jsx
+++ src/components/forms/RegisterForm.jsx
@@ -1,23 +1,12 @@
-import axios from 'axios';
 import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { registerUser } from '../../features/authSlice'
-import { Alert } from 'react-bootstrap';
-import { Link, useNavigate } from "react-router-dom";
-import BeatLoader from 'react-spinners/BeatLoader'
+import { Link } from "react-router-dom";
 
 const RegisterForm = () => {
 
     const [ fullname, setFullname ] = useState('');
     const [ email, setEmail ] = useState('');
     const [ password, setPassword ] = useState('');
-    const [ errorMessage, setErrorMessage ] = useState('');
-    const [ successMessage, setSuccessMessage ] = useState('');
-    const [ loading, setLoading ] = useState(false);
     const [ show, setShow ] = useState(false);
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const AppRoot = process.env.REACT_APP_API_ROOT;
 
     const handleShowPassword = () => {
         if(show) {
@@ -27,50 +16,18 @@ const RegisterForm = () => {
         }
     }
 
-    const handleRegister = async(e) => {
-        e.preventDefault();
-        setLoading(true)
-
-        try {
-            const { data } = await axios.post(`${AppRoot}/auth/register`, { fullname, email, password }, { withCredentials: true });
-
-            if (data.auth.status === 'failed') {
-                setLoading(false);
-                setErrorMessage(data.auth.message);
-                setTimeout(() => {
-                    setErrorMessage("");
-                }, 2000);
-                return;
-            } else {
-                setLoading(false);
-                setErrorMessage('');
-                setSuccessMessage(data.auth.message);
-                setTimeout(() => {
-                    dispatch(registerUser(data.user));
-                    navigate("/")
-                }, 2000);
-            }
-
-        } catch (error) {
-            console.log(error.message)
-        }
-
-    }
-
     return (
-        <form className="auth-form" onSubmit={handleRegister}>
+        <form className="auth-form" onSubmit={(e) => e.preventDefault()}>
             <h4 className="mt-0 mb-4 auth-heading text-center">CREATE ACCOUNT</h4>
-            <div className="mb-3"><label className="form-label">Name</label><input className="form-control" type="text" placeholder="e.g john doe" onChange={(e) => setFullname(e.target.value)} /></div>
-            <div className="mb-3"><label className="form-label">Email</label><input className="form-control" type="email" placeholder="e.g johndoe@gmail.com" onChange={(e) => setEmail(e.target.value)} /></div>
+            <div className="mb-3"><label className="form-label">Name</label><input className="form-control" type="text" placeholder="e.g john doe" value={fullname} onChange={(e) => setFullname(e.target.value)} /></div>
+            <div className="mb-3"><label className="form-label">Email</label><input className="form-control" type="email" placeholder="e.g johndoe@gmail.com" value={email} onChange={(e) => setEmail(e.target.value)} /></div>
             <div className="mb-4">
                 <div className="d-flex align-items-center justify-content-between mb-2"><label className="form-label mb-0">Password</label><button className="btn btn-sm text-light" type="button" onClick={handleShowPassword}>{show ? "hide" : "show"}</button></div>
-                <input className="form-control" type={show ? "text" : "password"} placeholder="xxxxxxxxxx" autoComplete="off" onChange={(e) => setPassword(e.target.value)} />
+                <input className="form-control" type={show ? "text" : "password"} placeholder="xxxxxxxxxx" autoComplete="off" value={password} onChange={(e) => setPassword(e.target.value)} />
             </div>
-            {loading ? <button className="btn btn-primary btn-lg w-100 mb-4 border-0 px-4" type="button" disabled><BeatLoader size={7} color={'#ffffff'} loading={loading} aria-label="Loading Spinner" data-testid="loader" /></button> : <button className="btn btn-primary btn-lg w-100 mb-4 border-0" type="submit">submit</button>}
-            {errorMessage && <Alert show={true} variant="danger">{ errorMessage }</Alert>} 
-            {successMessage && <Alert show={true} variant="success">{ successMessage }</Alert>}
+            <button className="btn btn-primary btn-lg w-100 mb-4 border-0" type="submit">submit</button>
             <div>
-                <a className="btn btn-light w-100 mb-3 border-0 py-3 d-none" role="button" href={`${AppRoot}/auth/google`} rel="noopener noreferrer">
+                <a className="btn btn-light w-100 mb-3 border-0 py-3 d-none" role="button" href={`/auth/google`} rel="noopener noreferrer">
                     <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16" className="bi bi-google me-3 mb-1">
                         <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
                     </svg>Signup with Google</a>
