e3ccd74aa168d2341b13df703049b5561e6a4dd9
unknown <codemonga@gmail.com>
2023-08-31 15:32:54 +0100

    modified the pricing page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/main/Home";
 import Movies from "./pages/main/Movies";
 import TvShows from "./pages/main/TvShows";
-// import Pricing from "./pages/main/Pricing";
+import Pricing from "./pages/main/Pricing";
 // import About from "./pages/main/About";
 // authentification pages routes
 // import Register from "./pages/auth/Register";
@@ -40,8 +40,8 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="movies" element={<Movies/>} />
           <Route path="tvshows" element={<TvShows/>} />
-          {/* <Route path="pricing" element={<Pricing/>} />
-          <Route path="about" element={<About/>} />
+          <Route path="pricing" element={<Pricing/>} />
+          {/* <Route path="about" element={<About/>} />
           <Route path="screen" element={<Screen/>} />
           <Route path="register" element={ <Register/> } />
           <Route path="login" element={ <Login/> } />
--- src/components/dataList/PriceList.jsx
+++ src/components/dataList/PriceList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const PriceList = () => {
   return (
@@ -23,7 +24,7 @@ const PriceList = () => {
         <p><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" className="me-1 mb-1 text-danger">
             <path d="M10 14L12 12M12 12L14 10M12 12L10 10M12 12L14 14M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
             </svg>4k available</p>
-        <div className="mt-4"><button className="btn btn-outline-primary btn-lg w-100" type="button">Subscribe</button></div>
+        <div className="mt-4"><Link className="btn btn-outline-primary btn-lg w-100" to="/register">Subscribe</Link></div>
             </div>
         </div>
         <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 align-self-center">
@@ -46,7 +47,7 @@ const PriceList = () => {
             <p><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" className="me-1 mb-1 text-danger">
                 <path d="M10 14L12 12M12 12L14 10M12 12L10 10M12 12L14 14M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
                 </svg>4k available</p>
-            <div className="mt-4"><button className="btn btn-primary btn-lg w-100 border-0" type="button">Subscribe</button></div>
+            <div className="mt-4"><Link className="btn btn-primary btn-lg w-100 border-0" to="/register">Subscribe</Link></div>
             </div>
         </div>
         <div className="col-sm-12 col-md-6 col-lg-6 col-xl-4 col-xxl-4 align-self-center">
@@ -69,7 +70,7 @@ const PriceList = () => {
             <p><svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="none" className="me-1 mb-1 text-success">
                 <path d="M9 12L11 14L15 10M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
                 </svg>4k available</p>
-            <div className="mt-4"><button className="btn btn-outline-primary btn-lg w-100" type="button">Subscribe</button></div>
+            <div className="mt-4"><Link className="btn btn-outline-primary btn-lg w-100" to="/register">Subscribe</Link></div>
             </div>
         </div>
     </div>
